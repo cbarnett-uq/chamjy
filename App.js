7a07c3c78bf7104c28fff06f81cabdc04e982482
Chris Barnett <cgdbarnett@gmail.com>
2022-09-07 15:53:34 +1000

    Reimplement main layout.

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Session from './components/pages/session';
-import { NavigationComponent } from './components/navigationComponent';
 import NavigationService from './services/navigationService';
 import { SafeAreaView } from 'react-native';
+import MainLayout from './components/mainLayout';
 
 export default function App() {
+  // Register all page components here
   NavigationService.register("session", React.createElement(Session));
 
   return (
     <SafeAreaView>
-      <NavigationComponent/>
+      <MainLayout/>
     </SafeAreaView>
   );
 }
